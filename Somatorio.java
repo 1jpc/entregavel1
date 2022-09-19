e49d50b244eb854e73ee9e00db65c0a75c65e5c1
1jpc <111007877+1jpc@users.noreply.github.com>
2022-09-19 15:34:56 -0300

    Create Somatorio.java

---
+++ Somatorio.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Somatorio {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int somatorio = a + b;
+        System.out.println(somatorio)
+    }
+}
